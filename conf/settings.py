fd25087f30f4dfe91df43048795811a5f3440e9a
gudtogo <earias_1995@hotmail.com>
2024-10-09 21:49:33 -0300

    Update req

--- conf/settings.py
+++ conf/settings.py
@@ -92,16 +92,17 @@ WSGI_APPLICATION = "conf.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default='postgres://localhost'),
+    "default": dj_database_url.config(
+        default='postgres://localhost', conn_max_age=600, ssl_require=True
+    )
 }
 
 if DEBUG:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
+    "default": dj_database_url.config(
+        default='postgres://localhost', conn_max_age=600, ssl_require=True
+    )
+}
 else:
     DATABASES = {
         "default": {
